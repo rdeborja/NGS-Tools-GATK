3cc75b5f2acd53435d310d921d67ef11a7124762
Richard de Borja <richard.deborja@gmail.com>
2015-09-01 13:45:24 -0400

    completed standalone wrapper for the CombineVariants program

--- bin/mergevcf.pl
+++ bin/mergevcf.pl
@@ -5,7 +5,7 @@
 
 ### HISTORY #######################################################################################
 # Version       Date            Developer           Comments
-# 0.01
+# 0.01          2015-09-01      rdeborja            initial development
 
 ### INCLUDES ######################################################################################
 use warnings;
@@ -14,11 +14,21 @@ use Carp;
 use Getopt::Long;
 use Pod::Usage;
 use NGS::Tools::GATK;
+use File::Find::Rule qw(find);
+use Data::Dumper;
+use File::ShareDir ':ALL';
+use HPF::PBS;
 
 ### COMMAND LINE DEFAULT ARGUMENTS ################################################################
 # list of arguments and default values go here as hash key/value pairs
 our %opts = (
-    files => undef
+    dir => './',
+    sample => undef,
+    output => '',
+    memory => 6,
+    reference => '/hpf/largeprojects/adam/local/reference/homosapiens/ucsc/hs37d5/fasta/hs37d5.fa',
+    java => '/hpf/tools/centos6/java/1.7.0/bin/java',
+    gatk => '/hpf/tools/centos6/gatk/2.8.1/GenomeAnalysisTK.jar'
     );
 
 ### MAIN CALLER ###################################################################################
@@ -41,7 +51,13 @@ sub main {
         \%opts,
         "help|?",
         "man",
-        "files|f=s"
+        "dir|d:s",
+        "sample|s=s",
+        "output|o:s",
+        "memory|m:i",
+        "reference|r:s",
+        "java:s",
+        "gatk:s"
         ) or pod2usage(64);
     
     pod2usage(1) if $opts{'help'};
@@ -54,6 +70,46 @@ sub main {
             }
         }
 
+    # create a default output filename if one is not passed as an argument
+    my $output;
+    if ('' eq $opts{'output'}) {
+        $output = join('.',
+            $opts{'sample'},
+            'vcf'
+            );
+        }
+    else {
+        $output = $opts{'output'};
+        }
+
+    # get the list of VCF files in a given diretory, search recursively
+    my @vcf_files = File::Find::Rule->file()->name('*.vcf')->in($opts{'dir'});
+    my $gatk = NGS::Tools::GATK->new();
+    my $gatk_combine_vcf_run = $gatk->combine_variants(
+        vcf => \@vcf_files,
+        output => $output,
+        sample => $opts{'sample'},
+        reference => $opts{'reference'},
+        memory => $opts{'memory'},
+        java => $opts{'java'},
+        gatk => $opts{'gatk'}
+        );
+
+    my $pbs = HPF::PBS->new();
+    my $template = 'submit_to_pbs.template';
+    my $template_dir = join('/',
+        dist_dir('HPF'),
+        'templates'
+        );
+    my @hold_for = ();
+    my $pbs_run = $pbs->create_cluster_shell_script(
+        command => $gatk_combine_vcf_run->{'cmd'},
+        jobname => join('_', $opts{'sample'}, 'gatk', 'combinevariants'),
+        template_dir => $template_dir,
+        template => $template,
+        memory => $opts{'memory'},
+        submit => 'false'
+        );
     return 0;
     }
 
@@ -72,6 +128,8 @@ B<mergevcf.pl> [options] [file ...]
     Options:
     --help          brief help message
     --man           full documentation
+    --dir           directory containing VCF files (default: ./)
+    --
 
 =head1 OPTIONS
 
@@ -85,6 +143,10 @@ Print a brief help message and exit.
 
 Print the manual page.
 
+=item B<--dir>
+
+Name of directory containing VCF files to search for (default: ./).
+
 =back
 
 =head1 DESCRIPTION
@@ -93,7 +155,7 @@ B<mergevcf.pl> Merge VCF files for a given sample into a single VCF file.
 
 =head1 EXAMPLE
 
-mergevcf.pl
+mergevcf.pl --dir /data/vcf
 
 =head1 AUTHOR
 
